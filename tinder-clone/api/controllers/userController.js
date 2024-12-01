805356f83ff7ddc96f34b5a0984195266366b304
Hasan <hasandursunx@gmail.com>
2024-12-01 19:31:59 +0300

    match,user controller and cloudinary configuration added

---
+++ tinder-clone/api/controllers/userController.js
@@ -0,0 +1,38 @@
+import cloudinary from "../config/cloudinary.js";
+import User from "../models/User.js";
+
+export const updateProfile = async (req,res) => {
+    try {
+        const {image, ...otherData} = req.body;
+
+        let updateData = otherData;
+
+        if(image){
+            if(image.startsWith("data:image")){
+                try {
+                    const uploadResponse = await cloudinary.uploader.upload(image);
+                    updateData.image = uploadResponse.secure_url;
+                } catch (error) {
+                    console.log('Error uploading image : ', uploadError);
+
+                    return res.status(400).json({
+                        succsess : false,
+                        message : "Error uploading image"
+                    })
+                }
+            }
+        }
+
+        const updatedUser = await User.findNByIdAndUpdate(req.user.id, updateData, {new : true});
+        res.status(200).json({
+            success: true,
+            user : updateData
+        })
+    } catch (error) {
+        console.log("Error in updateData",error)
+        res.status(500).json({
+            success : false,
+            message : 'Internal server error', 
+        })
+    }
+}
