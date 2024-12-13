3fd57d6262c83ade3c46227ebae5e283a2bdf955
Hasan <hasandursunx@gmail.com>
2024-12-13 20:15:57 +0300

    home page added

--- tinder-clone/client/src/pages/HomePage.jsx
+++ tinder-clone/client/src/pages/HomePage.jsx
@@ -1,11 +1,10 @@
-import { useAuthStore } from "../store/useAuthStore"
+import Sidebar from '../components/Sidebar'
 
 const HomePage = () => {
-    const { logout } = useAuthStore();
     return (
-        <div>
-            HomePage
-            <button onClick={logout}>Logout</button>
+        <div className="flex flex-col lg:flex-row min-h-screen bg-gradient-to-br from-pink-100 to-purple-100
+        overflow-hidden">
+            <Sidebar />
         </div>
     )
 }
